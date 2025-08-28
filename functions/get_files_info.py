fbdbea220cf6dff8e9278a2af126ceec14dff3a1
Aadit <aadits2004@gmail.com>
2025-08-28 18:54:57 +0530

    added get_files_info method

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,5 +1,21 @@
 import os
 
 
-def get_files_info(working_dir, dir="."):
-    os.path.join(working_dir, dir)
+def get_files_info(working_directory, directory="."):
+    full_path = os.path.join(working_directory, directory)
+    abs_full_path = os.path.abspath(full_path)
+    abs_working_directory = os.path.abspath(working_directory)
+
+    if not os.path.isdir(directory):
+        return f'Error: "{directory}" is not a directory'
+
+    if not abs_full_path.startswith(abs_working_directory + os.sep) and abs_full_path != abs_working_directory:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+    files_list = []
+    for file in os.path.listdir(abs_full_path):
+        file_size = os.path.getsize(file)
+        is_dir = os.path.isdir(file)
+        files_list.append(f"{file}: file_size:{file_size}, is_dir={is_dir}")
+
+    return abs_full_path
