681d22df8b504a375801a0aa81eeca35025c6a5b
Aadit <aadits2004@gmail.com>
2025-08-29 11:35:14 +0530

    get files info func working

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -6,16 +6,17 @@ def get_files_info(working_directory, directory="."):
     abs_full_path = os.path.abspath(full_path)
     abs_working_directory = os.path.abspath(working_directory)
 
-    if not os.path.isdir(directory):
-        return f'Error: "{directory}" is not a directory'
+    if not os.path.isdir(abs_full_path):
+        return f'Error: "{abs_full_path}" is not a directory'
 
     if not abs_full_path.startswith(abs_working_directory + os.sep) and abs_full_path != abs_working_directory:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
     files_list = []
-    for file in os.path.listdir(abs_full_path):
-        file_size = os.path.getsize(file)
-        is_dir = os.path.isdir(file)
-        files_list.append(f"{file}: file_size:{file_size}, is_dir={is_dir}")
+    for file in os.listdir(abs_full_path):
+        relative_full_path = os.path.join(abs_full_path, file)
+        file_size = os.path.getsize(relative_full_path)
+        is_dir = os.path.isdir(relative_full_path)
+        files_list.append(f"- {file}: file_size:{file_size}, is_dir={is_dir}")
 
-    return abs_full_path
+    return files_list
